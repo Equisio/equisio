c3683e6d899ceeb3243d2d9a6e59de39f1caaef4
sevilla <sevilla@ITP14319.sercor.itp.es>
2025-07-10 11:52:20 +0200

    Improved navbar design for better mobile and desktop experience

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  i18n: {
-    locales: ['en', 'es', 'fr', 'de', 'nl'],
-    defaultLocale: 'en',
-  },
+const { i18n } = require('./next-i18next.config')
+
+module.exports = {
+  i18n,
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
